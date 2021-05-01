64bfe59566a5fc1b76874c64741a9f6fd046a728
Jilan-2 <78647371+Jilan-2@users.noreply.github.com>
2021-05-01 12:17:01 +0700

    index.php
    Membuat tabel dan connect mysql.

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+$db = mysql_connect("localhost", "root", "", "alumni_smp")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Alumni SMP</title>
+</head>
+
+<body>
+  <h1>Daftar Alumni SMP Tanpa Nama</h1>
+  <table border="1" cellpadding="10" cellspacing="3">
+    <tr>
+        <th>No.</th>
+        <th>Nama</th>
+        <th>Warga</th>
+        <th>Angkatan</th>
+        <th>Sekolah lanjutan</th>
+        <th>Alamat</th>
+    </tr> 
+    <tr>
+        <th>1</th>
+        <th>Jilan Nabilah</th>
+        <th>6</th>
+        <th>57</th>
+        <th>SMANIS</th>
+        <th>Rumah kanan jalan</th>
+    </tr> 
+  </table> 
+</body>
+</html>
